8dfb0e5275d7e47f174065f4f89221f415b38108
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-09 12:09:03 +0300

    Add endpoints for posts, comments, groups

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,11 +1,17 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import PostViewSet
+from .views import CommentViewSet, GroupViewSet, PostViewSet
 
-router = DefaultRouter()
-router.register('posts', PostViewSet, basename='post')
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet, basename='post')
+router_v1.register('groups', GroupViewSet, basename='group')
+router_v1.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comment_post'
+)
 
 urlpatterns = [
-    path('v1/', include(router.urls))
+    path('v1/', include(router_v1.urls))
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,35 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from .serializers import PostSerializer
-from posts.models import Post
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from .mixin import OnlyAuthorMixinViewSet
+from posts.models import Group, Post
 
 
-class PostViewSet(ModelViewSet):
+class GroupViewSet(ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(OnlyAuthorMixinViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(OnlyAuthorMixinViewSet):
+    serializer_class = CommentSerializer
+
+    def get_post(self):
+        return get_object_or_404(Post, id=self.kwargs.get('post_id'))
+
+    def get_queryset(self):
+        return self.get_post().comments.all()
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post=self.get_post()
+        )
