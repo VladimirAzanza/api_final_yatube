56d4f3bca73856596ba9dd3ecf0f47def369e1fb
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-09 12:40:44 +0300

    Add a processing value function for image at post serializer

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,20 @@
+import base64
+
+from django.core.files.base import ContentFile
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
 
 from posts.models import Comment, Group, Post
+
+
+class Base64ImageField(serializers.ImageField):
+    def to_internal_value(self, data):
+        if isinstance(data, str) and data.startswith('data:image'):
+            format, imgstr = data.split(';base64,')
+            ext = format.split('/')[-1]
+            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
+        return super().to_internal_value(data)
 
 
 class GroupSerializer(serializers.ModelSerializer):
@@ -13,6 +25,7 @@ class GroupSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
+    image = Base64ImageField()
 
     class Meta:
         fields = '__all__'
