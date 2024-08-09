589fdc09490a6fa291af6be1f6341c4375e28a78
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-09 12:17:45 +0300

    Add jwt token authentification

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,5 +13,7 @@ router_v1.register(
 )
 
 urlpatterns = [
-    path('v1/', include(router_v1.urls))
+    path('v1/', include(router_v1.urls)),
+    path('v1/auth/', include('djoser.urls')),
+    path('v1/auth/', include('djoser.urls.jwt'))
 ]
