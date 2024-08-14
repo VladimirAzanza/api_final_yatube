5e2287cb484627fef63f55aabe95f4c025aa45e3
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-14 15:31:35 +0300

    Add custom field

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,19 +1,12 @@
-import base64
-
-from django.core.files.base import ContentFile
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from posts.models import Comment, Follow, Group, Post, User
+from .base64_image_field import Base64ImageField
+from posts.models import Comment, Follow, Group, Post
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-        return super().to_internal_value(data)
+User = get_user_model()
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -46,10 +46,14 @@ class Comment(models.Model):
 
 class Follow(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='follower'
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower'
     )
     following = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='following'
+        User,
+        on_delete=models.CASCADE,
+        related_name='following'
     )
 
     class Meta:
