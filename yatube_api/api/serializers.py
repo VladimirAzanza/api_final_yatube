01742bda108f3cd567f438f92c5a4dae8d3373fa
Vladimir Azanza <vladimirazanza@gmail.com>
2024-08-19 10:39:32 +0300

    Change file name to fields.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.validators import UniqueTogetherValidator
 
-from .base64_image_field import Base64ImageField
+from .fields import Base64ImageField
 from posts.models import Comment, Follow, Group, Post
 
 
